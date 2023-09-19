56f80240fd08ab3265c9390960066a202c778891
MannyWorkstation <cresp1el@cmich.edu>
2023-09-19 12:44:56 -0400

    updated code for the core.py

---
+++ scripts/run_analysis.py
@@ -0,0 +1,17 @@
+from neural_analysis_pkg.core import NeuralAnalysis
+
+# Define the path to your project folder
+project_folder_path ='/home/cresp1el-local/Documents/MATLAB/Data/lmc_project_v2'
+
+# Initialize the NeuralAnalysis class with the project folder path
+analysis = NeuralAnalysis(project_folder_path)
+
+# Print a message to confirm that the NeuralAnalysis object has been created
+print("NeuralAnalysis object created successfully.")
+
+# You can now use the methods of your class to perform analyses
+# For example, assuming you have methods like `process_dat_file` and `perform_analysis` in your class:
+# analysis.process_dat_file()
+# analysis.perform_analysis()
+
+# You might want to print some outputs or save results to files to verify that the analyses are working correctly
--- src/neural_analysis_pkg/core.py
+++ src/neural_analysis_pkg/core.py
@@ -14,81 +14,83 @@ class NeuralAnalysis:
         self.data = None
         self.good_channels = None
         self.noisy_channels = None
-
+        
     def process_dat_file(self, project_folder_path):
-        """
-        Iteratively process .dat files in the project folder.
-        
-            Processes a .dat file to identify noisy channels based on RMS values.
-    
-        Parameters:
-        - file_path: str, path to the .dat file
-        - n_channels: int, number of recording channels (default: 32)
-        - sampling_rate: int, data sampling rate in Hz (default: 32000)
-        - dtype: numpy dtype, data type of the samples (default: np.int16)
-        
-        Returns:
-        - noisy_channels: numpy array, indices of the noisy channels
-        - good_channels: numpy array, indices of the good channels
+	    """
+	    Iteratively process .dat files in the project folder.
+	    
+	    Processes a .dat file to identify noisy channels based on RMS values.
 
-        Parameters:
-        - project_folder_path: str, the path to the project folder containing all the group and recording folders
-        
-        """
-        
-        # Define the path to the SpikeStuff folder
-        spikestuff_path = os.path.join(project_folder_path, "SpikeStuff")
+	    Parameters:
+	    - project_folder_path: str, the path to the project folder containing all the group and recording folders
+	    
+	    """
+	    
+	    # Define the path to the SpikeStuff folder
+	    spikestuff_path = os.path.join(project_folder_path, "SpikeStuff")
 
-        # Loop through all groups in the SpikeStuff folder
-        for group_name in os.listdir(spikestuff_path):
-            group_path = os.path.join(spikestuff_path, group_name)
+	    # Loop through all groups in the SpikeStuff folder
+	    all_group_names = os.listdir(spikestuff_path)
+	    total_groups = len(all_group_names)
+	    print(f"Total groups: {total_groups}")
 
-            # Ensure it's a directory and not a file
-            if os.path.isdir(group_path):
+	    for group_index, group_name in enumerate(all_group_names):
+		group_path = os.path.join(spikestuff_path, group_name)
 
-                # Loop through all recordings in the group folder
-                for recording_name in os.listdir(group_path):
-                    recording_path = os.path.join(group_path, recording_name)
+		# Ensure it's a directory and not a file
+		if os.path.isdir(group_path):
 
-                    # Ensure it's a directory and not a file
-                    if os.path.isdir(recording_path):
+		    # Loop through all recordings in the group folder
+		    all_recording_names = os.listdir(group_path)
+		    total_recordings_in_group = len(all_recording_names)
+		    print(f"Group {group_index + 1}/{total_groups}: {group_name} has {total_recordings_in_group} recordings")
 
-                        # Define the path to the SUA folder within the current recording folder
-                        sua_path = os.path.join(recording_path, 'SUA')
+		    for recording_index, recording_name in enumerate(all_recording_names):
+		        recording_path = os.path.join(group_path, recording_name)
 
-                        # Check if the SUA directory exists before trying to access it
-                        if os.path.isdir(sua_path):
+		        # Ensure it's a directory and not a file
+		        if os.path.isdir(recording_path):
 
-                            # Loop through all files in the SUA folder to find .dat files
-                            for file_name in os.listdir(sua_path):
-                                if file_name.endswith(".dat"):
-                                    dat_file_path = os.path.join(sua_path, file_name)
-                                    
-                                    # Now, dat_file_path is the path to a .dat file
-                                    # Call your .dat file processing function here
-                                        # Step 1: Read the .dat file
-                                    data = np.fromfile(dat_file_path, dtype=np.int16) #dtype=np.int16 by default
-                                    reshaped_data = data.reshape((-1, self.n_channels))
-                                    
-                                    # Step 2: Compute the RMS value for each channel
-                                    rms_values = np.sqrt(np.mean(np.square(reshaped_data), axis=0))
-                                    
-                                    # Step 3: Identify the 1st and 3rd quartiles of the RMS values
-                                    q1 = np.percentile(rms_values, 25)
-                                    q3 = np.percentile(rms_values, 75)
-                                    iqr = q3 - q1
-                                    
-                                    # Step 4: Mark channels as excessively noisy
-                                    lower_bound = q1 - 3 * iqr
-                                    upper_bound = q3 + 3 * iqr
-                                    
-                                    noisy_channels = np.where((rms_values < lower_bound) | (rms_values > upper_bound))[0]
-                                    good_channels = np.setdiff1d(np.arange(self.n_channels), noisy_channels)
+		            # Define the path to the SUA folder within the current recording folder
+		            sua_path = os.path.join(recording_path, 'SUA')
 
-                                    print(f"Found .dat file: {dat_file_path}")  # For testing
-                        else:
-                            print(f"No SUA directory found in {recording_path}")  # For testing
-                            
+		            # Check if the SUA directory exists before trying to access it
+		            if os.path.isdir(sua_path):
+
+		                # Loop through all files in the SUA folder to find .dat files
+		                for file_name in os.listdir(sua_path):
+		                    if file_name.endswith(".dat"):
+		                        dat_file_path = os.path.join(sua_path, file_name)
+		                        
+		                        # Now, dat_file_path is the path to a .dat file
+		                        # Call your .dat file processing function here
+		                            # Step 1: Read the .dat file
+		                        data = np.fromfile(dat_file_path, dtype=self.dtype)
+		                        reshaped_data = data.reshape((-1, self.n_channels))
+		                        
+		                        # Log the shape and data type of the loaded data
+		                        print(f"Data shape: {reshaped_data.shape}, Data type: {reshaped_data.dtype}")
+
+		                        # Step 2: Compute the RMS value for each channel
+		                        rms_values = np.sqrt(np.mean(np.square(reshaped_data), axis=0))
+		                        
+		                        # Step 3: Identify the 1st and 3rd quartiles of the RMS values
+		                        q1 = np.percentile(rms_values, 25)
+		                        q3 = np.percentile(rms_values, 75)
+		                        iqr = q3 - q1
+		                        
+		                        # Step 4: Mark channels as excessively noisy
+		                        lower_bound = q1 - 3 * iqr
+		                        upper_bound = q3 + 3 * iqr
+		                        
+		                        noisy_channels = np.where((rms_values < lower_bound) | (rms_values > upper_bound))[0]
+		                        good_channels = np.setdiff1d(np.arange(self.n_channels), noisy_channels)
+
+		                        print(f"Processing recording {recording_index + 1}/{total_recordings_in_group} in group {group_index + 1}/{total_groups}")
+		            else:
+		                print(f"No SUA directory found in {recording_path}")  # For testing
+	      
+		                    
     #def read_dat_file(self):
         # code to read the .dat file and set self.data
 
