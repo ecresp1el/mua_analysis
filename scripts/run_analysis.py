464a1f8a9dfefa25094251601217aa41a3c45c93
ecresp1el <cresp1el@cmich.edu>
2023-09-19 12:59:03 -0400

    added the process_dat_methods

--- scripts/run_analysis.py
+++ scripts/run_analysis.py
@@ -9,6 +9,9 @@ analysis = NeuralAnalysis(project_folder_path)
 # Print a message to confirm that the NeuralAnalysis object has been created
 print("NeuralAnalysis object created successfully.")
 
+#call the process_dat_file method and pass the project folder path
+analysis.process_dat_file(project_folder_path)
+
 # You can now use the methods of your class to perform analyses
 # For example, assuming you have methods like `process_dat_file` and `perform_analysis` in your class:
 # analysis.process_dat_file()
