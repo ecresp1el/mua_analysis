47b6f1cefe0f9b43453a8c15309ab7a9bd7600f7
MannyWorkstation <cresp1el@cmich.edu>
2023-09-20 13:50:03 -0400

    nyquist issues

--- src/neural_analysis_pkg/core.py
+++ src/neural_analysis_pkg/core.py
@@ -282,9 +282,12 @@ class NeuralAnalysis:
         - order: int, the order of the Butterworth filter (default: 3)
         """
         # Define the bandpass filter using butter
-        nyq = 0.5 * 10000  # Nyquist frequency after downsampling the data to 10 kHz in the previous step
+        nyq = 0.5 * (self.sampling_rate / 3)  # Nyquist frequency based on the downsampled rate
+        bandpass_high = 4999  # A value safely below the Nyquist frequency
         low = bandpass_low / nyq
         high = bandpass_high / nyq
+
+        
 
         # Define highpass and lowpass filters separately
         b_high, a_high = butter(order, low, btype='high')
